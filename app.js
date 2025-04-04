71fdffdc39ed66f2ac2f2cb702822767bc2cf646
Ludvig-D <74721111+Ludvig-D@users.noreply.github.com>
2025-04-04 18:53:06 +0200

    Made it so the buttons work now

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 function add(num1, num2) {
-  return num1 + num2;
+  return parseFloat(num1) + parseFloat(num2);
 }
 
 function subtract(num1, num2) {
-  return num1 - num2;
+  return parseFloat(num1) - parseFloat(num2);
 }
 
 function multiply(num1, num2) {
-  return num1 * num2;
+  return parseFloat(num1) * parseFloat(num2);
 }
 
 function divide(num1, num2) {
-  return num1 / num2;
+  return parseFloat(num1) / parseFloat(num2);
 }
 
 function operate(num1, operator, num2) {
+  console.log(num1, operator, num2);
   if (operator === '+') {
-    add(num1, num2);
+    return add(num1, num2);
   } else if (operator === '-') {
-    subtract(num1, num2);
+    return subtract(num1, num2);
   } else if (operator === 'x') {
-    multiply(num1, num2);
+    return multiply(num1, num2);
   } else if (operator === '÷') {
-    divide(num1, num2);
+    return divide(num1, num2);
   } else {
     console.error('Something went wrong in operate');
   }
@@ -31,15 +32,64 @@ function operate(num1, operator, num2) {
 const buttons = document.querySelectorAll('button');
 const currNum = document.querySelector('.currNum');
 
+let operator = '';
+let num1 = '';
+let opBool = false;
+
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
     if (parseInt(e.target.innerText) || e.target.innerText === '0') {
-      currNum.innerText = currNum.innerText + e.target.innerText;
+      if (opBool === true) {
+        currNum.innerText = e.target.innerText;
+        opBool = false;
+      } else {
+        if (currNum.innerText === '0') {
+          currNum.innerText = e.target.innerText;
+        } else {
+          currNum.innerText = currNum.innerText + e.target.innerText;
+        }
+      }
     } else if (e.target.innerText == 'DEL' || e.target.innerText == 'AC') {
       if (e.target.innerText === 'DEL') {
-        currNum.innerText = currNum.innerText.slice(0, -1);
+        if (currNum.innerText.length == 1) {
+          currNum.innerText = 0;
+        } else {
+          currNum.innerText = currNum.innerText.slice(0, -1);
+        }
       } else if (e.target.innerText === 'AC') {
-        currNum.innerText = '';
+        currNum.innerText = '0';
+      }
+    } else if (e.target.innerText === '.') {
+    } else {
+      if (operator != '') {
+        num1 = operate(num1, operator, currNum.innerText);
+        currNum.innerText = num1;
+        operator = '';
+      }
+      if (e.target.innerText === '+') {
+        operator = '+';
+        num1 = currNum.innerText;
+        opBool = true;
+      } else if (e.target.innerText === '-') {
+        operator = '-';
+        num1 = currNum.innerText;
+        opBool = true;
+      } else if (e.target.innerText === 'x') {
+        operator = 'x';
+        num1 = currNum.innerText;
+        opBool = true;
+      } else if (e.target.innerText === '÷') {
+        operator = '÷';
+        num1 = currNum.innerText;
+        opBool = true;
+      } else if (e.target.innerText === '=') {
+        if (operator == '') {
+          console.error('Has to have a operator!!');
+        } else {
+          num1 = operate(num1, operator, currNum.innerText);
+          currNum.innerText = num1;
+          operator = '';
+        }
       }
     }
   });
